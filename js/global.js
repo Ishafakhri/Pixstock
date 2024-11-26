9b589d2280e2f758f285b53d01b4b1c0419eb07e
Ishafakhri Akbar <ishafakhri303@gmail.com>
2024-11-27 02:17:01 +0700

    add theme module and implement ripple effect for interactive elements

--- js/global.js
+++ js/global.js
@@ -1,6 +1,31 @@
-/**
- * @copyright codewithsadee 2023
- * @author sadee <codewithsadee@gmail.com>
- */
-
 "use strict";
+/**
+ * Import
+ */
+import { ripple } from "./utils/ripple.js";
+
+
+
+
+
+/**
+ * Header on-scroll state
+ */
+document.addEventListener("DOMContentLoaded", () => {
+  const $header = document.querySelector("[data-header]");
+
+  if ($header) {
+    window.addEventListener("scroll", () => {
+      $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+    });
+  } else {
+    console.error("Element with [data-header] not found.");
+  }
+});
+
+/**
+ * Add ripple effect to elements
+ */
+const /** {NodeElement} */ $rippleElems = document.querySelectorAll("[data-ripple]");
+
+$rippleElems.forEach($rippleElem => ripple($rippleElem));
